2940d6e7620af78b00db94a2d01d17d428cfc7d5
Richard Huss <richard@huss.org.uk>
2026-05-24 15:11:52 +0100

    Move API key from config to secrets

--- src/upgrades.ts
+++ src/upgrades.ts
@@ -1,16 +1,31 @@
-import type { CompanionStaticUpgradeScript } from '@companion-module/base'
-import type { ModuleConfig } from './config.js'
+import type {
+	CompanionStaticUpgradeScript,
+	CompanionUpgradeContext,
+	CompanionStaticUpgradeResult,
+	CompanionStaticUpgradeProps,
+} from '@companion-module/base'
+import type { ModuleConfig, ModuleSecrets } from './config.js'
 
-export const UpgradeScripts: CompanionStaticUpgradeScript<ModuleConfig>[] = [
+export const UpgradeScripts: CompanionStaticUpgradeScript<ModuleConfig, ModuleSecrets>[] = [
 	/*
 	 * Place your upgrade scripts here
 	 * Remember that once it has been added it cannot be removed!
 	 */
-	// function (context, props) {
-	// 	return {
-	// 		updatedConfig: null,
-	// 		updatedActions: [],
-	// 		updatedFeedbacks: [],
-	// 	}
-	// },
+	function (
+		context: CompanionUpgradeContext<ModuleConfig>,
+		_props: CompanionStaticUpgradeProps<ModuleConfig, ModuleSecrets>,
+	): CompanionStaticUpgradeResult<ModuleConfig, ModuleSecrets> {
+		return {
+			updatedConfig: {
+				// Key will be removed - moving to secrets
+			},
+			updatedSecrets: {
+				// Migrate existing API key from config to secrets
+				// Ensure it's a string, even if it was undefined
+				key: context.currentConfig.key?.toString() || '',
+			},
+			updatedActions: [],
+			updatedFeedbacks: [],
+		}
+	},
 ]
